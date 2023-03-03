e83bd242fc190b29855fdcda1b311f790baad11c
Matthew <mbrooks9622@gmail.com>
2023-03-03 14:51:36 +0000

    initial commit

---
+++ react-portfolio/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from './About'
+import Contact from "./Contact"
+
+function Home() {
+  return (
+    <div>
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default Home;
